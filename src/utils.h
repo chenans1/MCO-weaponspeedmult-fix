51cd83aeaee5c47d89825ef168a22a239370580e
Ansel Chen <73325489+chenans1@users.noreply.github.com>
2026-02-02 18:11:01 -0500

    feat(utils): add normalization of wsm
    technically this should already be handled by comprehensive attack speed patch from noahboddie but I'm checking this here again just in case users don't bother reading

--- src/utils.h
+++ src/utils.h
@@ -1,9 +1,22 @@
 #pragma once
+#include <cmath>
 
 namespace utils {
     //check for lowercase equality
     inline bool ieq(char a, char b) noexcept {
         return std::tolower((unsigned char)a) == std::tolower((unsigned char)b);
+    }
+
+    inline float normWSM(float wsm) noexcept {
+        /*guard against 0/negative weaponspeedmult values (IIRC this is fixed by noahboddie's attackspeed SKSE patch)
+        but just in case:*/
+        if (wsm == 0.0f || wsm == 1.0f) {
+            return 1.0f;
+        }
+        if (!std::isfinite(wsm) || wsm < 0.0f) {
+            return 1.0f;
+        }
+        return wsm;
     }
 
     bool istarts_with(std::string_view s, std::string_view prefix) noexcept;
