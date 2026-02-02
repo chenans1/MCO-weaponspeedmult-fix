819c505b6a442fbc044c35e2f4b089fa45ae54d9
Ansel Chen <73325489+chenans1@users.noreply.github.com>
2026-02-02 15:48:38 -0500

    feat: initial attempt at hooking activate
    seems to work

--- src/AnimationHandler.cpp
+++ src/AnimationHandler.cpp
@@ -1,6 +1,23 @@
 #include "PCH.h"
 #include "AnimationHandler.h"
 
-namespace weaponspeedmultFix {
+using namespace SKSE;
+using namespace SKSE::log;
+using namespace SKSE::stl;
 
+namespace weaponspeedmultFix {
+    void hkbHook::Install() { 
+        log::info("[hkbHook]: attempt to install hooks");
+        REL::Relocation<std::uintptr_t> vtblhkbClipGenerator{RE::VTABLE_hkbClipGenerator[0]};
+        _originalActivate = vtblhkbClipGenerator.write_vfunc(0x4, Activate);
+        log::info("[hkbHook]: installed Activate hook at slot 0x4");
+    }
+
+    void hkbHook::Activate(RE::hkbClipGenerator* self, const RE::hkbContext& a_context) {
+        if (self) {
+            const char* name = self->animationName.c_str();
+            log::info("[hkbClipGenerator::Activate] clip='{}' speed={}", name ? name : "<null>", self->playbackSpeed);
+        }
+        _originalActivate(self, a_context);
+    }
 }
--- src/AnimationHandler.h
+++ src/AnimationHandler.h
@@ -1,5 +1,12 @@
 #pragma once
 
 namespace weaponspeedmultFix {
-	
+    class hkbHook {
+    public:
+        static void Install();
+
+    private:
+        static void Activate(RE::hkbClipGenerator* self, const RE::hkbContext& a_context);
+        static inline REL::Relocation<decltype(Activate)> _originalActivate;
+    };
 }
