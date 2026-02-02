011c5c9214bfe250427dec5f5237d554679626cc
Ansel Chen <73325489+chenans1@users.noreply.github.com>
2026-02-02 16:14:00 -0500

    feat(AnimationHandler): log if clip name matches
    avoid spam, test to see if the util functions and IsMCOAttackClip() works

--- src/AnimationHandler.cpp
+++ src/AnimationHandler.cpp
@@ -1,11 +1,29 @@
 #include "PCH.h"
 #include "AnimationHandler.h"
+#include "utils.h"
+#include <array>
 
 using namespace SKSE;
 using namespace SKSE::log;
 using namespace SKSE::stl;
 
 namespace weaponspeedmultFix {
+    static bool IsBFCOAttackClip(std::string_view clipPath) noexcept {
+        const auto file = utils::basename_view(clipPath);
+        return utils::istarts_with(file, "BFCO_attack") || utils::istarts_with(file, "BFCO_PowerAttack");
+    }
+
+    static bool IsMCOAttackClip(std::string_view clipPath) noexcept {
+        static constexpr std::array<std::string_view, 5> kPrefixes = {
+            "MCO_attack", "MCO_powerattack", "MCO_sprintattack", "MCO_sprintpowerattack", "MCO_weaponart"};
+
+        const auto file = utils::basename_view(clipPath);
+        for (auto p : kPrefixes) {
+            if (utils::istarts_with(file, p)) return true;
+        }
+        return false;
+    }
+
     void hkbHook::Install() { 
         log::info("[hkbHook]: attempt to install hooks");
         REL::Relocation<std::uintptr_t> vtblhkbClipGenerator{RE::VTABLE_hkbClipGenerator[0]};
@@ -16,7 +34,11 @@ namespace weaponspeedmultFix {
     void hkbHook::Activate(RE::hkbClipGenerator* self, const RE::hkbContext& a_context) {
         if (self) {
             const char* name = self->animationName.c_str();
-            log::info("[hkbClipGenerator::Activate] clip='{}' speed={}", name ? name : "<null>", self->playbackSpeed);
+            /*log::info("[hkbClipGenerator::Activate] clip='{}' speed={}", name ? name : "<null>", self->playbackSpeed);*/
+            if (IsMCOAttackClip(name)) {
+                log::info("[hkbClipGenerator::Activate] clip='{}' speed={}", name ? name : "<null>",
+                          self->playbackSpeed);
+            }
         }
         _originalActivate(self, a_context);
     }
