7e8905f44df6c22a2b92ac77243ece6053c91495
Ansel Chen <73325489+chenans1@users.noreply.github.com>
2026-02-02 18:11:50 -0500

    feat(hkbHook): implemented weaponspeedmult application
    as far as I can tell this justworks TM*

--- src/AnimationHandler.cpp
+++ src/AnimationHandler.cpp
@@ -9,8 +9,13 @@ using namespace SKSE::stl;
 
 namespace weaponspeedmultFix {
     static bool IsBFCOAttackClip(std::string_view clipPath) noexcept {
+        static constexpr std::array<std::string_view, 5> kPrefixes = {
+            "BFCO_attack", "BFCO_powerattack", "BFCO_SpecialAttack", "BFCO_SpecialAttackPower", "MCO_weaponart"};
         const auto file = utils::basename_view(clipPath);
-        return utils::istarts_with(file, "BFCO_attack") || utils::istarts_with(file, "BFCO_PowerAttack");
+        for (auto p : kPrefixes) {
+            if (utils::istarts_with(file, p)) return true;
+        }
+        return false;
     }
 
     static bool IsMCOAttackClip(std::string_view clipPath) noexcept {
@@ -22,6 +27,15 @@ namespace weaponspeedmultFix {
             if (utils::istarts_with(file, p)) return true;
         }
         return false;
+    }
+
+    //test from asrak's magicutils: -0xC0 pointer offset
+    [[nodiscard]] inline RE::BShkbAnimationGraph* GraphFromCharacter(RE::hkbCharacter* chr) noexcept {
+        if (!chr) {
+            return nullptr;
+        }
+        //from BShkbAnimationGraph.h: hkbCharacter characterInstance;// 0C0
+        return SKSE::stl::adjust_pointer<RE::BShkbAnimationGraph>(chr, -0xC0);
     }
 
     void hkbHook::Install() { 
@@ -51,17 +65,39 @@ namespace weaponspeedmultFix {
     }
 
     void hkbHook::Update(RE::hkbClipGenerator* self, const RE::hkbContext& a_context, float a_timestep) {
-        _originalUpdate(self, a_context, a_timestep);
         if (!self) {
-            log::warn("[hkbHook::Update]: no self");
-            return;
+            log::warn("[hkbHook::Update] no self");
+            return _originalUpdate(self, a_context, a_timestep);
         }
-        //fetch anim name
+
         const char* raw = self->animationName.c_str();
         if (!raw || !IsMCOAttackClip(raw)) {
-            return;
+            return _originalUpdate(self, a_context, a_timestep);
         }
-        log::info("[hkbHook::Update] clip='{}' speed={}", raw ? raw : "<null>", self->playbackSpeed);
-        return;
+        //log::info("[hkbHook::Update] clip='{}' speed={}", raw ? raw : "<null>", self->playbackSpeed);
+        auto* graph = GraphFromCharacter(a_context.character);
+        if (!graph) {
+            log::warn("[hkbHook::Update]: No graph");
+            return _originalUpdate(self, a_context, a_timestep);
+        }
+        if (graph->behaviorGraph != a_context.behavior) {
+            return _originalUpdate(self, a_context, a_timestep);
+        }
+        
+        auto* actor = graph->holder;
+        if (!actor) {
+            log::warn("[hkbHook::Update]: No actor");
+            return _originalUpdate(self, a_context, a_timestep);
+        }
+
+        // fetch weapon speed mult actor value
+        const float wsm = utils::normWSM(actor->AsActorValueOwner()->GetActorValue(RE::ActorValue::kWeaponSpeedMult));
+        const float base = self->playbackSpeed;
+        self->playbackSpeed = base * wsm;
+        log::info("[hkbHook::Update] clip='{}', base={}, weaponspeedmult={}, updated speed={}",
+            raw ? raw : "<null>", base,  wsm, self->playbackSpeed);
+
+        //call original function
+        return _originalUpdate(self, a_context, a_timestep);
     }
 }
