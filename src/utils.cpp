fe77c677f92479b20e01477ee6575f5d08afa334
Ansel Chen <73325489+chenans1@users.noreply.github.com>
2026-02-02 16:21:34 -0500

    minor fixes and output path change

--- src/utils.cpp
+++ src/utils.cpp
@@ -2,7 +2,7 @@
 #include "utils.h"
 
 namespace utils {
-    bool istarts_with(std::string_view s, std::string_view prefix) {
+    bool istarts_with(std::string_view s, std::string_view prefix) noexcept {
         if (s.size() < prefix.size()) return false;
 
         for (size_t i = 0; i < prefix.size(); ++i) {
@@ -12,7 +12,7 @@ namespace utils {
     }
 
     //clip names appear as 'Animations\MCO_attack1.hkx' -> extract MCO_attack
-    std::string_view basename_view(std::string_view path) {
+    std::string_view basename_view(std::string_view path) noexcept {
         const auto p1 = path.find_last_of('\\');
         const auto p2 = path.find_last_of('/');
         const auto pos = (p1 == std::string_view::npos) ? p2 : (p2 == std::string_view::npos) ? p1 : (std::max)(p1, p2);
